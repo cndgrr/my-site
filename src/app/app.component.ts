9ee5ad68e643b0b49db92085f252dd19c1f3dfc9
Daniel Marin <danielarturomt@gmail.com>
2021-04-16 20:42:05 -0300

    style: add new colors and enhance looks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,11 @@ import { Component } from '@angular/core';
     <header class="header">MENU</header>
     <main>
       <app-welcome></app-welcome>
-      <app-about></app-about>
-      <app-skills></app-skills>
-      <app-cover-letter></app-cover-letter>
+      <div class="content">
+        <app-about></app-about>
+        <app-skills></app-skills>
+        <app-cover-letter></app-cover-letter>
+      </div>
     </main>
   `,
   styleUrls: ['./app.component.scss'],
