244db36504623d6d66a54ca373ce98000c868a65
Daniel Marin <danielarturomt@gmail.com>
2021-04-16 15:15:23 -0300

    feat(welcome): set up backbone of welcome section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: 'CINDY GUERRA SITE',
-  styles: [],
+  template: `
+    <header class="header">MENU</header>
+    <main>
+      <section class="welcome">
+        <div>
+          <p class="welcome__title">
+            Hi, <br />
+            I’m <b>Cindy</b> <br />
+            <b>Guerra</b>.
+          </p>
+          <button class="welcome__cta">Ask for Quote</button>
+        </div>
+        <figure class="welcome__image">
+          <img
+            srcset="
+              assets/images/cindy-large.png 1000w,
+              assets/images/cindy-small.png  300w
+            "
+          />
+        </figure>
+      </section>
+      <section class="about">ABOUT</section>
+    </main>
+  `,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
