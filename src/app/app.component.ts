44c98c8e9cc6a7fc4aff6d43bef115460a48be91
Daniel Marin <danielarturomt@gmail.com>
2021-04-16 12:30:08 -0300

    chore: setting up the basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: 'CINDY GUERRA SITE',
+  styles: [],
 })
-export class AppComponent {
-  title = 'cindy-guerra-site';
-}
+export class AppComponent {}
