59699978caaf6dc18b54b6c672bc3a594dfadb8c
Daniel Marin <danielarturomt@gmail.com>
2021-04-17 15:13:52 -0300

    feat: add social networks at the bottom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,26 @@ import { Component } from '@angular/core';
         <app-experiences></app-experiences>
         <app-facts></app-facts>
       </div>
+      <ul class="social-networks">
+        <li>
+          <a target="_blank" href="https://github.com/cndgrr">
+            <img src="assets/images/github.png" alt="Go to my github" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="#">
+            <img src="assets/images/twitter.png" alt="Go to my twitter" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/cindy-guerra-mendez/"
+          >
+            <img src="assets/images/linkedin.png" alt="Go to my linkedin" />
+          </a>
+        </li>
+      </ul>
     </main>
   `,
   styleUrls: ['./app.component.scss'],
