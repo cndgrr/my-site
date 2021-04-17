518635a849424af9d9dca9d443850e7c96a45d06
Daniel Marin <danielarturomt@gmail.com>
2021-04-17 13:33:35 -0300

    feat: toggle feature for experiences

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,33 +1,40 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
-import { faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 import { Experience } from '../core/interfaces/experience.interface';
 
 @Component({
   selector: 'app-experience',
   template: `
     <article class="experience">
-      <button class="experience__header">
+      <button class="experience__header" (click)="onToggle()">
         <h3 class="experience__title">{{ experience.title }}</h3>
-        <div class="experience__trigger">
-          <p class="experience__subtitle">{{ experience.subtitle }}</p>
-          <fa-icon
-            [icon]="faCaretUp"
-            size="lg"
-            class="experience__icon"
-          ></fa-icon>
-        </div>
+        <fa-icon
+          [icon]="faCaretDown"
+          size="lg"
+          class="experience__icon"
+          [ngClass]="{ reverse: isOpen }"
+        ></fa-icon>
       </button>
-      <ul class="experience__content">
-        <li *ngFor="let task of experience.tasks">
-          {{ task }}
-        </li>
-      </ul>
+      <div class="experience__content" *ngIf="isOpen">
+        <p class="experience__subtitle">{{ experience.subtitle }}</p>
+        <ul>
+          <li *ngFor="let task of experience.tasks">
+            {{ task }}
+          </li>
+        </ul>
+      </div>
     </article>
   `,
   styleUrls: ['./experience.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExperienceComponent {
-  faCaretUp = faCaretUp;
+  faCaretDown = faCaretDown;
+  isOpen = false;
+
   @Input() experience!: Experience;
+
+  onToggle() {
+    this.isOpen = !this.isOpen;
+  }
 }
